64c9c73ea8f8ee0ad3e5a3758a4e92373b257620
kimmoonwoong <ansdnd1500@naver.com>
2023-01-29 19:50:51 +0900

    feature: 커스텀 데이터 클래스 생성

---
+++ moonwoongkim/Custom_Dataset.py
@@ -0,0 +1,26 @@
+import torchvision.datasets as datasets
+import torchvision.transforms as transforms
+
+from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, Dataset
+
+class Custom_Dataset(Dataset):
+    def __init__(self, X, y, train_mode=True, transforms=None):
+        self.X = X
+        self.y = y
+        self.train_mode = train_mode
+        self.transforms = transforms
+
+    def __getitem__(self, index):
+        X = self.X[index]
+
+        if self.transforms is not None:
+            X = self.transforms(X)
+
+        if self.train_mode:
+            y = self.y[index]
+            return X,y
+        else:
+            return X
+    def __len__(self):
+        return len(self.X)
