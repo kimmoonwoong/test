d18b2d51e5caed94d4b15f282340fd24def8c61b
kimmoonwoong <ansdnd1500@naver.com>
2023-03-29 00:51:54 +0900

    commit

--- moonwoongkim/data_setting.py
+++ moonwoongkim/data_setting.py
@@ -27,7 +27,7 @@ os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
 
 def FSDDataset(train_set):          # wav데이터 변환
-    label_setting_fsd = {"Bark": 2, "Meow": 3, "Bus": 5, "Squeak": 5, "Knock": 5} # 새롭게 클래스 라벨링
+    label_setting_fsd = {"Bark": 1} # 새롭게 클래스 라벨링
     FSD_train_dataset_path = 'C:\\Users\\user\\OneDrive - koreatech.ac.kr\\FSDKaggle2018.audio_train'
     data_lock_count = {} # 테스트용으로 각 라벨마다 100개씩 데이터를 뽑아 냄
     for index, row in dataset.iterrows():
@@ -36,7 +36,7 @@ def FSDDataset(train_set):          # wav데이터 변환
         if class_label in label_setting_fsd:  # 데이터 셋에 우리가 쓸 데이터를 골라내는 작업
 
             label_check = []            # 나중에 학습할 때 loss계산을 위해 라벨링 형태를 맞춰줌
-            for j in range(0, 8):
+            for j in range(0, 6):
                 if j + 1 == label_setting_fsd[class_label]:
                     label_check.append(1)
                 else:
@@ -52,8 +52,7 @@ def FSDDataset(train_set):          # wav데이터 변환
 def UrBanDataset(train_set):    # wav파일 변환
     Urbandataset = pd.read_csv('C:\\Users\\user\\OneDrive - koreatech.ac.kr\\UrbanSound8K\\UrbanSound8K\\metadata\\UrbanSound8K.csv')
     Urban_train_dataset_path = 'C:\\Users\\user\\OneDrive - koreatech.ac.kr\\UrbanSound8K\\UrbanSound8K\\audio'
-    label_setting_UrBan = {"car_horn": 1, "dog_bark": 2, "siren": 4, "street_music": 5, "drilling": 5,
-                           "air_conditioner": 5, "jachammer": 5}    # 새롭게 라벨링
+    label_setting_UrBan = {"car_horn": 1, "dog_bark": 2, "siren": 3}    # 새롭게 라벨링
     for index, row in Urbandataset.iterrows():
         class_label = row['class']
         if class_label in label_setting_UrBan:  # 우리가 쓸 데이터를 골라내는 작업
@@ -61,7 +60,7 @@ def UrBanDataset(train_set):    # wav파일 변환
             fold_number = row['fold']
             file_path = Urban_train_dataset_path + '\\' + 'fold' + str(fold_number) + '\\' + file_name
             label_check = []        # 나중에 학습할 때 loss계산을 위해 라벨링 형태를 맞춰줌
-            for j in range(0, 8):
+            for j in range(0, 6):
                 if j + 1 == label_setting_UrBan[class_label]:
                     label_check.append(1)
                 else:
@@ -77,7 +76,7 @@ def AI_HubDataset(train_set):
     Ai_Hub_dataset_path = 'C:\\Users\\user\\OneDrive - koreatech.ac.kr\\도시소리'
     Ai_Hub_type_path = ['자동차', '이륜자동차', '동물']
     Ai_Hub_labelset = 'C:\\Users\\user\\OneDrive - koreatech.ac.kr\\교통소음'
-    label_setting_aihub = {"차량경적": 1, "차량주행음": 5, "차량사이렌": 4, "이륜차경적": 1, "이륜차주행음": 5, "개": 2, "고양이": 3} # 새롭게 라벨링
+    label_setting_aihub = {"차량경적": 1, "차량사이렌": 3, "이륜차경적": 1, "개": 2} # 새롭게 라벨링
     for s in Ai_Hub_type_path:
         path = Ai_Hub_labelset + '\\' + str(s)
         filelist = os.listdir(path)
@@ -94,7 +93,7 @@ def AI_HubDataset(train_set):
                 class_label = str(jsondata["annotations"][0]["categories"]["category_03"])
                 if class_label in label_setting_aihub: # 우리가 쓸 데이터를 뽑아 냄
                     label_check = []    # 나중에 학습할 때 loss계산을 위해 라벨링 형태를 맞춰줌
-                    for j in range(0, 8):
+                    for j in range(0, 6):
                         if j + 1 == label_setting_aihub[class_label]:
                             label_check.append(1)
                         else:
@@ -110,8 +109,8 @@ def AI_HubAlertDataset(train_set):
     AI_HubAlert_dataset_path = 'C:\\Users\\user\\OneDrive - koreatech.ac.kr\\경보소리'
     AI_HubAlert_type_path = ["도난경보", "화재경보", "비상경보"]
     AI_HubAlert_label_path = "C:\\Users\\user\\OneDrive - koreatech.ac.kr\\경보소리라벨링\\경보"
-    label_setting_ai_hubAleart = {"도난경보 소리": 8, "도난 경보음 소리": 8, "침입감지 경보 소리": 8, "화재경보 소리":7, "화재 경보 소리": 7,
-                                  "가스누설 화재경보 소리": 7, "자동차 경적 소리": 1, "비상경보 소리": 6, "철도 건널목 신호음 소리": 6, "민방위훈련 사이렌 소리": 6, "공습경보 소리" : 6} # 새롭게 라벨링
+    label_setting_ai_hubAleart = {"도난경보 소리": 6, "도난 경보음 소리": 6, "침입감지 경보 소리": 6, "화재경보 소리":5, "화재 경보 소리": 5,
+                                  "가스누설 화재경보 소리": 5, "자동차 경적 소리": 1, "비상경보 소리": 4, "철도 건널목 신호음 소리": 4, "민방위훈련 사이렌 소리": 4, "공습경보 소리" : 4} # 새롭게 라벨링
     for s in AI_HubAlert_type_path:
         print(s)
         path = AI_HubAlert_label_path + "\\" + str(s)
@@ -126,7 +125,7 @@ def AI_HubAlertDataset(train_set):
                 class_label = str(jsondata["LabelDataInfo"]["Desc"])
 
                 label_check = []        # 나중에 학습할 때 loss계산을 위해 라벨링 형태를 맞춰줌
-                for j in range(0, 8):
+                for j in range(0, 6):
                     if j + 1 == label_setting_ai_hubAleart[class_label]:
                         label_check.append(1)
                     else:
@@ -137,17 +136,67 @@ def AI_HubAlertDataset(train_set):
     print("데이터 생성 완료")
     return train_set
 
+def AI_HubAlertsoundArgumentDataset(train_set):
+    AI_HubAlert_dataset_path = 'E:\\경보소리데이터변환'
+    AI_HubAlert_type_path = ["도난경보", "화재경보", "비상경보"]
+    AI_HubAlert_label_path = "C:\\Users\\user\\OneDrive - koreatech.ac.kr\\경보소리라벨링\\경보"
+    label_setting_ai_hubAleart = {"도난경보 소리": 6, "도난 경보음 소리": 6, "침입감지 경보 소리": 6, "화재경보 소리":5, "화재 경보 소리": 5,
+                                  "가스누설 화재경보 소리": 5, "자동차 경적 소리": 1, "비상경보 소리": 4, "철도 건널목 신호음 소리": 4, "민방위훈련 사이렌 소리": 4, "공습경보 소리" : 4} # 새롭게 라벨링
+    argumentype = ["_pitch_down", '_pitch_up', '_speed_down', "_speed_up"]
+    speed_down = []
+    speed_up = []
+    for s in AI_HubAlert_type_path:
+        print(s)
+        path = AI_HubAlert_label_path + "\\" + str(s)
+        fill_list = os.listdir(path)
+        for filename in fill_list:
+            class_label = ""
+            file_path = path + "\\" + filename
+            with open(file_path, 'rt', encoding='UTF8') as file:
+                jsondata = json.load(file)
+                for types in argumentype:
+                    data_file_path = AI_HubAlert_dataset_path + "\\" + str(s) + '변환' + "\\" + str(
+                        jsondata["LabelDataInfo"]["LabelID"]) + types + ".wav"
+                    class_label = str(jsondata["LabelDataInfo"]["Desc"])
+
+                    label_check = []        # 나중에 학습할 때 loss계산을 위해 라벨링 형태를 맞춰줌
+                    for j in range(0, 6):
+                        if j + 1 == label_setting_ai_hubAleart[class_label]:
+                            label_check.append(1)
+                        else:
+                            label_check.append(0)
+                    data, sr = librosa.load(data_file_path, sr=22050)       # librosa 모델을 사용하여 fft
+                    if types == '_speed_up':
+                        speed_up.append([data, label_check])
+                    elif types == '_speed_down':
+                        speed_down.append([data, label_check])
+                    else:
+                        train_set.append([data, label_check])                   # 데이터 저장
+    up_index = len(train_set)
+    for data in speed_up:
+        train_set.append(data)
+    down_index = len(train_set)
+    for data in speed_down:
+        train_set.append(data)
+    print("데이터 생성 완료")
+    return train_set, up_index, down_index
 train_data_label = []
 
 
-def extract_feature(data, label, isCheck):      # 위에서 변환한 데이터를 mfcc의 이용으로 소리 데이터를 벡터화 시킴( 벡터화 시킴으로써 CNN모델을 사용할 수 있게 됨 )
+def extract_feature(data, isCheck, up_index, down_index):      # 위에서 변환한 데이터를 mfcc의 이용으로 소리 데이터를 벡터화 시킴( 벡터화 시킴으로써 CNN모델을 사용할 수 있게 됨 )
     mfccs = []
     slice = lambda a, i: a[:, 0:i] if a.shape[1] > i else np.hstack((a, np.zeros((a.shape[0], i - a.shape[1]))))    # 데이터의 길이를 설정한 길이에 맞게 맞춰줌
     index = 0           # 한 군데에서 가져온 데이터가 앞에 10가량이 무슨 소리인지 소개하는 소리이기에 짜르기 위해 그 데이터를 찾아냄
     for i in data:
         mfcc = librosa.feature.mfcc(y=i, sr=22050, n_mfcc=40, n_fft=400)    # mfcc를 통해 벡터화를 시킴
-        if index >= isCheck:            # 앞서 말한 데이터부터 10초가량을 짜름
+        if index >= isCheck and index < up_index:            # 앞서 말한 데이터부터 10초가량을 짜름
             mfcc = mfcc[:, 1100:]
+            index += 1
+        elif index >= up_index and index < down_index:
+            mfcc = mfcc[:, 600:]
+            index += 1
+        elif index >= down_index:
+            mfcc = mfcc[:, 1400:]
         else:
             index += 1
         mfcc = slice(mfcc, 80)     # 설정한 길이에 맞게 맞춰주는 작업
@@ -165,6 +214,7 @@ train_data_set = UrBanDataset(train_data_set)
 train_data_set = AI_HubDataset(train_data_set)
 isCheck = len(train_data_set)
 train_data_set = AI_HubAlertDataset(train_data_set)
+train_data_set, up_index, down_indx = AI_HubAlertsoundArgumentDataset(train_data_set)
 print(len(train_data_set))
 train_data_set = pd.DataFrame(train_data_set, columns=['data', 'label'])
 # f = open("data_setcheck.csv", "w")
@@ -174,7 +224,7 @@ train_data_set = pd.DataFrame(train_data_set, columns=['data', 'label'])
 #     write.writerows(str(train_data_set.data[i].tolist()))
 # f.close()
 train_x = np.array(train_data_set.data)
-trains_mfcc = extract_feature(train_x, train_data_set.label, isCheck)   #fft된 데이터를 mfcc를 적용시켜 벡터화 시킴
+trains_mfcc = extract_feature(train_x, isCheck, up_index, down_indx)   #fft된 데이터를 mfcc를 적용시켜 벡터화 시킴
 trains_mfcc = np.array(trains_mfcc)
 trains_mfcc = trains_mfcc.reshape(-1, trains_mfcc.shape[1], trains_mfcc.shape[2], 1)
 train_X = trains_mfcc
@@ -253,7 +303,7 @@ class CNNclassification(torch.nn.Module): # 4중 layer로 구현
              nn.MaxPool2d(kernel_size=2, stride=2))  # pooling layer
 
          self.fc_layer = nn.Sequential(
-             nn.Linear(200 * 5 * 1, 8)  # fully connected layer(ouput layer)
+             nn.Linear(200 * 5 * 1, 6)  # fully connected layer(ouput layer)
          )
 
      def forward(self, x):
@@ -372,7 +422,7 @@ for fold, (train_index, vail_index) in enumerate(skf.split(train_X, test_y)):
     for i in train_index:
         train_data_test_X.append(train_X[i])
         new_label = []
-        for label_index in range(0,8):
+        for label_index in range(0,6):
             if label_index == test_y[i]:
                 new_label.append(1)
             else:
@@ -385,7 +435,7 @@ for fold, (train_index, vail_index) in enumerate(skf.split(train_X, test_y)):
     for i in vail_index:
         vail_data_test_X.append(train_X[i])
         new_label = []
-        for label_index in range(0,8):
+        for label_index in range(0,6):
             if label_index == test_y[i]:
                 new_label.append(1)
             else:
